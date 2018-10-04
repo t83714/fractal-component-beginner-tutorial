5127ef431879f906834d1e2aeb8ecede83f4b5be
Jacky Jiang <t83714@gmail.com>
2018-10-05 00:32:26 +1000

    Complete tutorial

--- src/RandomGifPair/index.js
+++ src/RandomGifPair/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { AppContainerUtils, AppContainer } from "fractal-component";
-import RandomGif, { actions as randomGifActions, actionTypes as randomGifActionTypes } from "../RandomGif";
+import { AppContainerUtils, AppContainer, ActionForwarder } from "fractal-component";
+import RandomGif, { actions as RandomGifActions, actionTypes as RandomGifActionTypes } from "../RandomGif";
+
+import * as actions from "./actions";
+import * as actionTypes from "./actions/types";
+import reducer from "./reducers";
+import saga from "./sagas";
 
 import jss from "jss";
 import jssDefaultPreset from "jss-preset-default";
@@ -10,10 +15,26 @@ import styles from "./styles";
 class RandomGifPair extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            itemsLoading: {},
+            isLoading: false,
+            error: null
+        };
 
         this.componentManager = AppContainerUtils.registerComponent(this, {
             namespace: "io.github.t83714/RandomGifPair",
+            reducer,
+            saga,
+            /**
+             * Register actions for action serialisation / deserialisation.
+             * It's much easier to use Symbol as action type to make sure no action type collision among different component.
+             * ( Considering we now use actions as primary way for inter-component communication, it's quite important in a multicaset action environment)
+             */
+            actionTypes,
+            // --- specify accepted types of external multicast actions
+            // --- By default, component will not accept any incoming multicast action.
+            // --- No limit to actions that are sent out
+            allowedIncomingMulticastActionTypes: [actionTypes.REQUEST_NEW_PAIR],
             namespaceInitCallback: componentManager => {
                 const styleSheet = jss
                     .setup(jssDefaultPreset())
@@ -62,10 +83,7 @@ class RandomGifPair extends React.Component {
                         <button
                             onClick={() => {
                                 this.componentManager.dispatch(
-                                    randomGifActions.requestNewGif(),
-                                    // --- sent multicast actions 
-                                    // --- and flow down to all namespace paths under `${this.componentManager.fullPath}/Gifs/`
-                                    "./Gifs/*"
+                                    actions.requestNewPair()
                                 );
                             }}
                             disabled={this.state.isLoading}
@@ -76,6 +94,31 @@ class RandomGifPair extends React.Component {
                         </button>
                     </div>
                 )}
+                {/**
+                 * Use ActionForwarder to throw NEW_GIF out of RandomGifPair container
+                 * Set namespace to `${this.componentManager.fullPath}/Gifs` in order to listen to
+                 * all `out of box` actions from two `RandomGif` components
+                 */}
+                <ActionForwarder
+                    namespacePrefix={`${this.componentManager.fullPath}/Gifs`}
+                    pattern={RandomGifActionTypes.NEW_GIF}
+                    relativeDispatchPath="../../../../*"
+                />
+
+                {/**
+                 * Use ActionForwarder to forward LOADING_START & LOADING_COMPLETE actions from `RandomGif`
+                 * to current component (`RandomGifPair`)'s namespace.
+                 * i.e. from `${this.componentManager.fullPath}/Gifs` to `${this.componentManager.fullPath}`
+                 * Thus, `relativeDispatchPath` should be ".."
+                 */}
+                <ActionForwarder
+                    namespacePrefix={`${this.componentManager.fullPath}/Gifs`}
+                    pattern={action =>
+                        action.type === RandomGifActionTypes.LOADING_START ||
+                        action.type === RandomGifActionTypes.LOADING_COMPLETE
+                    }
+                    relativeDispatchPath=".."
+                />
             </div>
         );
     }
@@ -93,3 +136,22 @@ RandomGifPair.defaultProps = {
 };
 
 export default RandomGifPair;
+
+//--- actions component may send out
+const exposedActionTypes = {
+    // --- export NEW_GIF action type as well just
+    // --- so people can use `RandomGifPair` without knowing `RandomGif`
+    NEW_GIF: RandomGifActionTypes.NEW_GIF,
+    LOADING_START: actionTypes.LOADING_START,
+    LOADING_COMPLETE: actionTypes.LOADING_COMPLETE,
+    REQUEST_NEW_PAIR: actionTypes.REQUEST_NEW_PAIR
+};
+//--- action component will accept
+const exposedActions = {
+    requestNewPair: actions.requestNewPair
+};
+
+/**
+ * expose actions for component users
+ */
+export { exposedActionTypes as actionTypes, exposedActions as actions };
